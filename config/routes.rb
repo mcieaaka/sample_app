0d68c9032d938d29ce02319dfed8554a9ec9de3a
Harshit Srivastava <harshit0300@gmail.com>
2022-07-20 00:41:21 +0530

    sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,27 @@
 Rails.application.routes.draw do
-  
   root 'static_pages#home'
-  # get 'static_pages/home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  # get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get 'sessions/new'
+  
+  # root 'static_pages#home'
+  # # get 'static_pages/home'
+  # get '/help', to: 'static_pages#help'
+  # get '/about', to: 'static_pages#about'
+  # get '/contact', to: 'static_pages#contact'
+  # get '/signup', to: 'users#new'
+  # resources :users
+  # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # # Defines the root path route ("/")
+  # # root "articles#index"
 
-  # root "application#hello"
+  # # root "application#hello"
 end
