77488190ea118cba700cdf29b8d99906b5e56408
Harshit Srivastava <harshit0300@gmail.com>
2022-07-21 23:10:29 +0530

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   # get '/login', to: 'sessions#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  # delete '/logout', to: 'sessions#destroy'
+  get '/logout', to:'sessions#destroy'
   resources :users
   # get 'sessions/new'
   
--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,3 +1,10 @@
 const { webpackConfig } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+webpackConfig.plugins.prepend('Provide', new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+    })
+)
+
 module.exports = webpackConfig
