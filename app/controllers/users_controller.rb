2fad9c3b55af0f8b1f4a38ad142350015fa0305b
Harshit Srivastava <harshit0300@gmail.com>
2022-07-24 01:32:14 +0530

    Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit,:update]
+  before_action :logged_in_user, only: [:index,:edit,:update]
   before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users =User.all;
+  end
 
   def show
     @user = User.find(params[:id])
@@ -9,9 +13,6 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-  end
-
-  def edit
   end
 
   def create
