d251e647a60c344624c024c2594e6910b5564db9
Harshit Srivastava <harshit0300@gmail.com>
2022-07-27 01:00:36 +0530

    microposts controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :microposts, dependent: :destroy
     # before_save { self.email = email.downcase }
     attr_accessor :remember_token, :activation_token,:reset_token
     before_save { email.downcase! }
